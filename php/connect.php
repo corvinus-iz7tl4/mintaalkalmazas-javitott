a3c41d1c8865b5ce61bbc2d98c1484ddd1b6babb
iz7tl4 <blanka.vago@stud.uni-corvinus.hu>
2023-04-30 23:51:42 +0200

    connect to correct db

--- php/connect.php
+++ php/connect.php
@@ -12,17 +12,17 @@ header("Content-type: text/html; charset=utf-8");
 <body>
     <h1>
         <?php
-        $adatbazis = "iz7tl4";
+        $adatbazis = "javitott_iz7tl4";
         $kapcsolat = mysqli_connect("127.0.0.1", "root", "");
         if (!$kapcsolat) {
             $mysqli;
             die("Nem lehet csatlakozni a MySQL kiszolgálóhoz! " . mysqli_error($mysqli));
         } else {
-            print "Sikerült csatlakozni <br>";
+            //print "Sikerült csatlakozni <br>";
         }
         mysqli_select_db($kapcsolat, $adatbazis)
             or die("Nem lehet megnyitni a következő adatbázist: $adatbazis" . mysqli_error($mysqli));
-        print "Sikeresen kiválasztott adatbázis: " . $adatbazis . "<br>";
+        //print "Sikeresen kiválasztott adatbázis: " . $adatbazis . "<br>";
         ?>
     </h1>
 </body>
