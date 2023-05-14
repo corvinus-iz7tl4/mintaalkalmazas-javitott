5467d1fed644a85095d82d3fcf6450646643cdf5
iz7tl4 <blanka.vago@stud.uni-corvinus.hu>
2023-05-14 23:04:31 +0200

    add favicon

--- php/login.php
+++ php/login.php
@@ -8,6 +8,7 @@ header("Content-type: text/html; charset=utf-8");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" href="../css/képek/favicon.png" type="image/x-icon">
     <title>Bejelentkezés</title>
 </head>
 
